953952add74036f1337cda96b48255ed9681b219
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-04 06:44:07 +0700

    HW_Task 1_0_attempt_4

--- Lesson_1/Home work/HW_1_0/Program.cs
+++ Lesson_1/Home work/HW_1_0/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-a = 5; b = 7->max = 7
-a = 2 b = 10->max = 10
-a = -9 b = -3->max = -3
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7->max = 7
+//a = 2 b = 10->max = 10
+//a = -9 b = -3->max = -3
 
 Console.WriteLine("Введите 2 числa: ");
 string s_a = Console.ReadLine()!;
@@ -11,7 +11,7 @@ int a = int.Parse(s_a);
 int b = int.Parse(s_b);
 if (a > b)
 {
-    Console.Write(a+"= max", b + "= min");
+    Console.Write(a+" = max", b+" = min");
 }
 else if (a == b)
 {
@@ -19,5 +19,5 @@ else if (a == b)
 }
 else
 {
-    Console.Write(a+"= min", b+"= max");
+    Console.Write(a+" = min", b+" = max");
 }
