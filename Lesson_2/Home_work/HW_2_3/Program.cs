71d4fa2f2c3359ac4f99fa4348dbaf5635a1496c
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-05 11:27:16 +0700

    Task HW_2_3

--- Lesson_2/Home_work/HW_2_3/Program.cs
+++ Lesson_2/Home_work/HW_2_3/Program.cs
@@ -4,6 +4,11 @@
 //7 -> да
 //1 -> нет
 
-
-
-//Console.WriteLine("Hello, World!");
+void Weekend(int num)
+{
+    string text = "not quite a week";
+    if (num == 7 || num == 6) text = "weekend";
+    else if (num < 6 && num > 0) text = "workday";
+    Console.WriteLine($"{num} -> {text}");
+}
+Weekend(int.Parse(Console.ReadLine()!));
