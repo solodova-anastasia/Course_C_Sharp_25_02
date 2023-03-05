83b2ad6a1b958e8666538e21f35daebffc1b32f6
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-05 09:47:41 +0700

    Task HW_2_1

---
+++ Lesson_2/Home_work/HW_2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int TakeNum(int num)
+{
+Console.WriteLine(num);
+return ((num/100)*10+(num % 10));
+}
+
+
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
+
---
+++ Lesson_2/Home_work/HW_2_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int TakeNum(int num)
+{
+if (num % 100 == 0)
+Console.WriteLine(num);
+else 
+return num % 10;
+}
+
+
+int result = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(result);
+
+
+
+//Console.WriteLine("Hello, World!");
