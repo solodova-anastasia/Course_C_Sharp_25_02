44d917c00ccdacc34d47ff4abcbd23fc1ed3d284
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-08 19:30:07 +0700

    Task HW_4-Conditions

---
+++ Lesson_4/Home work/HW_4_0/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// ЧЕРНОВИК!
+Console.WriteLine("Введите число A");
+Console.WriteLine("Введите число B");
+int A = int.Parse(Console.Readline());
+int B = int.Parse(Console.Readline());
+int result = A;
+{
+        for (int i = 1; i <= B; i++)
+        {
+            result = result * A;
+        }
+    Console.WriteLine("a в степени b = " + result);
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = NumVStepen(A);
+Console.WriteLine(result);
---
+++ Lesson_4/Home work/HW_4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+//ЧЕРНОВИК!!
+int SumNum(int num)
+{
+    int all_sum = 0;
+    for (int i = 1; i <= num; i++)
+        all_sum += i;
+    
+    return all_sum;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
