7b14af7a5763b3cb053515d80ee034131d1c65d9
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-15 18:10:25 +0700

    Tasks - HW_5_0

--- Lesson_5/5_3/Program.cs
+++ Lesson_5/5_3/Program.cs
@@ -3,38 +3,40 @@
 
 void Print(int[] arr)
 {
-int size = arr.Length;
+    int size = arr.Length;
 
-for (int i = 0; i < size; i++)
-Console.Write($"{arr[i]} ");
-Console.WriteLine();
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
 }
 
 int[] MassNums(int size, int from, int to)
 {
-int[] arr = new int[size];
+    int[] arr = new int[size];
 
-for (int i = 0; i < size; i++)
-arr[i] = new Random().Next(from, to);
-return arr;
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
 }
 
-int SumPosNeg(int[] arr)
-{
-int num_2 = 0;
-
-for (int i = 0; i < arr.Length; i++)
-{
-if (10 <= arr[i] && arr[i] <= 99)
-num_2++;
-}
-return num_2;
+int InRange(int[] arr)
+{   
+    int n = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)        
+            n += 1;          
+    }
+    return n;    
 }
 
 
 int num = int.Parse(Console.ReadLine()!);
 int start = int.Parse(Console.ReadLine()!);
 int stop = int.Parse(Console.ReadLine()!);
+
 int[] mass = MassNums(num, start, stop);
 Print(mass);
-Console.WriteLine (SumPosNeg(mass));
+Console.WriteLine(InRange(mass));
--- Lesson_5/Home Work/HW_5_0/Program.cs
+++ Lesson_5/Home Work/HW_5_0/Program.cs
@@ -1,3 +1,42 @@
-﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+int InRange(int[] arr)
+{
+    int n = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] <= 999 && arr[i] >= 100 && arr[i] % 2 != 1)
+            n += 1;
+    }
+    return n;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+Console.WriteLine(InRange(mass));
