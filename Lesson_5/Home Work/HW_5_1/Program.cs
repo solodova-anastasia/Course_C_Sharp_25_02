a64d23b19f23b593f3f6c05c059cc7803067f7ad
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-15 18:38:37 +0700

    Tasks - HW_5_1

--- Lesson_5/Home Work/HW_5_1/Program.cs
+++ Lesson_5/Home Work/HW_5_1/Program.cs
@@ -1,4 +1,43 @@
-﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+//стоящих на нечётных позициях.
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
 
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+void SumPos(int[] arr)
+{
+    int even_pos;
+    even_pos = 0;
+
+    for (int i = 1; i < arr.Length; i+=2)
+    {
+       even_pos += arr[i];
+    }
+    Console.WriteLine($"Сумма чисел, стоящих на нечётных позициях: {even_pos}");
+} 
+
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+SumPos(mass);
