4aed6112a69e803226cbc3ce96a4af4b070c40d4
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-15 20:21:09 +0700

    Tasks - HW_5_2

--- Lesson_5/Home Work/HW_5_2/Program.cs
+++ Lesson_5/Home Work/HW_5_2/Program.cs
@@ -1,3 +1,44 @@
 ﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
 
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+int[] PairsNum(int[] arr)
+{
+    int size = arr.Length;
+    int flex_size = size / 2 + size % 2;
+    int[] new_arr = new int[flex_size];
+
+    for (int i = 0; i < size / 2; i++)
+        new_arr[i] = arr[i] * arr[size - i - 1];
+
+    if (size % 2 == 1)
+        new_arr[flex_size - 1] = arr[size / 2];
+    return new_arr;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int[] arr_new = PairsNum(mass);
+Print(arr_new);
+
