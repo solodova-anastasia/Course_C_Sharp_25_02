0c9f6ba6ebbfeef98d58f7712b4bbc8409622721
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-15 23:16:54 +0700

    Task - 6_3

--- Lesson_6/6_2/Program.cs
+++ Lesson_6/6_2/Program.cs
@@ -1,5 +1,24 @@
 ﻿// 2. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
+string Binary(int num)
+{
+string res = "";
+
+while (num > 0)
+{
+res = num % 2 + res;
+num /= 2;
+}
+return res;
+}
+
+// 12 - 1100, 4 - 100, 45 - 101101
+int n = int.Parse(Console.ReadLine()!);
+string result = Binary(n);
+Console.WriteLine(result);
+
+
+// Вариант 2 
 string convert(int num_1)
 {
 string rezult = "";
---
+++ Lesson_6/6_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// 3. Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+
+void Fibnch (int num)
+{
+int A = 0;
+int B = 1;
+for (int i = 0; i < num; i++){
+Console.Write(A+" ");
+(A, B) = (B, A+B);
+}
+}
+int G = int.Parse(Console.ReadLine()!);
+Fibnch(G);
