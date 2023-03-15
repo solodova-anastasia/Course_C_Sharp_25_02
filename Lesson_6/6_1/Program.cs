71671dc330c82e6fdd211f5ea21413a86a092339
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-15 21:57:22 +0700

    Task - 6_2

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. Напишите программу, которая принимает на вход три числа
+// и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+void Trngl (int A, int B, int C)
+{
+if (A < B + C && B < A + C && C < A + B)
+Console.WriteLine($"Да, такой треугольник может быть");
+else Console.WriteLine($"Нет, такого треугольника не может быть");
+}
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+int num_3 = int.Parse(Console.ReadLine()!);
+
+Trngl(num_1, num_2, num_3);
---
+++ Lesson_6/6_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+string convert(int num_1)
+{
+string rezult = "";
+while (num_1 > 0)
+{
+rezult = num_1 % 2 + rezult;
+num_1 = num_1 / 2;
+}
+return rezult;
+}
+Console.WriteLine (convert(12));
