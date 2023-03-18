939b039388b8af15665877d1397df8cbb3bf94c5
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-18 12:21:08 +0700

    Task - 5_4-attemtt1

--- Lesson_5/Home Work/HW_5_2/Program.cs
+++ Lesson_5/Home Work/HW_5_2/Program.cs
@@ -3,42 +3,43 @@
 
 void Print(int[] arr)
 {
-    int size = arr.Length;
+int size = arr.Length;
 
-    for (int i = 0; i < size; i++)
-        Console.Write($"{arr[i]} ");
-    Console.WriteLine();
+for (int i = 0; i < size; i++)
+Console.Write($"{arr[i]} ");
+Console.WriteLine();
 }
 
 int[] MassNums(int size, int from, int to)
 {
-    int[] arr = new int[size];
+int[] arr = new int[size];
 
-    for (int i = 0; i < size; i++)
-        arr[i] = new Random().Next(from, to);
-    return arr;
+for (int i = 0; i < size; i++)
+arr[i] = new Random().Next(from, to);
+return arr;
 }
 
-int[] PairsNum(int[] arr)
+void SumPosNeg(int[] arr, int num_1, min1, max1)
+min1 = 0
 {
-    int size = arr.Length;
-    int flex_size = size / 2 + size % 2;
-    int[] new_arr = new int[flex_size];
-
-    for (int i = 0; i < size / 2; i++)
-        new_arr[i] = arr[i] * arr[size - i - 1];
-
-    if (size % 2 == 1)
-        new_arr[flex_size - 1] = arr[size / 2];
-    return new_arr;
+for (int i = 0; i < arr.Length; i++)
+{
+if (int i < min)
+{
+Console.WriteLine("minimum");
+return;
 }
+}
+max1 = 0
+
+Console.WriteLine("maximum");
+}
+
 
 int num = int.Parse(Console.ReadLine()!);
 int start = int.Parse(Console.ReadLine()!);
 int stop = int.Parse(Console.ReadLine()!);
-
+int num_1 = int.Parse(Console.ReadLine()!);
 int[] mass = MassNums(num, start, stop);
 Print(mass);
-int[] arr_new = PairsNum(mass);
-Print(arr_new);
-
+SumPosNeg(mass, num_1);
