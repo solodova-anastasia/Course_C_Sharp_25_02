aa19d5df6d95167f6907dcaab9b9d7596955577a
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-18 14:04:38 +0700

    Task 7_1

---
+++ Lesson_7/1_0/Program.cs
@@ -0,0 +1,36 @@
+﻿// 0. Задайте двумерный массив размером m×n, 
+//    заполненный случайными целыми числами. 
+
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
---
+++ Lesson_7/1_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// 1. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+//    Выведите полученный массив на экран.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = i + j;
+
+    return arr;
+}
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
