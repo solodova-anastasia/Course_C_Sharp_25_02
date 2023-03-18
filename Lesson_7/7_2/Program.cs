65bc649c2bc093923aeb6bddf65a670d64889e51
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-18 14:50:12 +0700

    Task 7_2

---
+++ Lesson_7/7_2/Program.cs
@@ -0,0 +1,48 @@
+﻿// 2. Задайте двумерный массив. Найдите элементы,
+//    у которых обе позиции чётные, и замените эти элементы на их квадраты.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+void Example (int [,] array)
+{
+  for (int i = 1; i < array.GetLength(0); i = i + 2)
+  {
+    for (int j = 1; j < array.GetLength(1); j += 2)
+    {
+      array[i, j] = array[i,j] * array[i,j];
+    }
+  }
+}
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
+Example(mass);
+Print(mass);
