df7ce124991fc8c802fff27584715f6eb36346d2
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-19 21:47:23 +0700

    Task HW_6_0

---
+++ Lesson_6/Home Work/HW_6_0/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int EnterNumber(int num)
+{
+    int count = 0;
+
+    for (int i = 0; i <= num; i++)
+    {
+        Console.Write($"Число {i}: ");
+        int a = int.Parse(Console.ReadLine()!);
+        if (a > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Сколько чисел будете вводить?");
+int a = int.Parse(Console.ReadLine()!);
+int result = EnterNumber(a-1);
+Console.WriteLine($"Пользователь ввёл {result} число(а) больше 0.");
