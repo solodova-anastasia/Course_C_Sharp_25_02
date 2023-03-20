f156d91fe4cdd4247b2d059e589cd2ac98e66bb4
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-20 08:34:33 +0700

    Task HW_6_1

--- Lesson_6/Home Work/HW_6_1/Program.cs
+++ Lesson_6/Home Work/HW_6_1/Program.cs
@@ -2,18 +2,27 @@
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-void CrossLine(double b1, double k1, double b2, double k2)
+void Intersection(double b1, double k1, double b2, double k2)
 {
-    if (k1 == k2 && b1 == b2)
-        Console.WriteLine("Прямые совпадают");
-
-    else if (k1 == k2 || b1 == b2)
-        Console.WriteLine("Прямые параллельны");
-
-    else
+    double k_sub = k1 - k2;
+    if (k_sub != 0)
     {
-        double crosss_X =
-    
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точки пересечения: {x}, {y}");
+    }
+    else if (k1 == k2 && b1 == b2)
+        Console.WriteLine("Существует бесконечное количество точек пересечения");
+    else
+        Console.WriteLine("Прямые параллельны, точек пересечения не существует");
 }
-    return;
-}
+
+Console.Write("Введите значение b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение b2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+Intersection(b1, k1, b1, k2);
