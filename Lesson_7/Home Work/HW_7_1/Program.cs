dfb0a8009c488ed75e451b83aa3db35c03f601eb
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-22 17:50:47 +0700

    Tasks HW_7_1

--- Lesson_7/Home Work/HW_7_1/Program.cs
+++ Lesson_7/Home Work/HW_7_1/Program.cs
@@ -6,4 +6,51 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
 
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+string Position(int[,] array, int num);
+int pos1 = 0;
+int pos2 = 0;
+{
+    if (array[i, j] == num)
+        pos1 = i + 1 && pos2 = j + 1;
+    Console.WriteLine($"{num}");
+}
+ Console.WriteLine("такого элемента в массиве не существует");
+
+Console.WriteLine("Enter amount of rows: ");
+int num_row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter amount of columns: ");
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int pos1 = int.Parse(Console.ReadLine()!);
+int pos2 = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row, num_column, start, stop, pos1, pos2);
+Print(mass);
+int num = int.Parse(Console.ReadLine()!);
+string sum = Position(mass, num);
--- Lesson_7/Home Work/HW_7_2/Program.cs
+++ Lesson_7/Home Work/HW_7_2/Program.cs
@@ -5,3 +5,53 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+void Print(double[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(n_new.NextDouble() * (from + to) - from, 2);
+
+    return arr;
+}
+
+void Average (int i, int j);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+     middle = sum / (i+1);
+}
+Console.ReadLine();
+
+Console.WriteLine("Enter amount of rows: ");
+int num_row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter amount of columns: ");
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
+string summ = Average(mass, middle);
+Console.WriteLine($"Среднее арифметическое элементов в каждом столбце: {middle}");
