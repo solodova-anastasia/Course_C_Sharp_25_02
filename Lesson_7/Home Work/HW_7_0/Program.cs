50a1df221ec08685b5f171fd6fa4d7b17d58537f
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-22 07:40:47 +0700

    Tasks HW_7_0

--- Lesson_7/Home Work/HW_7_0/Program.cs
+++ Lesson_7/Home Work/HW_7_0/Program.cs
@@ -4,3 +4,36 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+void Print(double[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(n_new.NextDouble()*(from+to)-from,2);
+
+    return arr;
+}
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
