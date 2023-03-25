d9e61547722b49e8fcb32015ef50784056249414
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-26 01:58:00 +0700

    Task HW_8_1

---
+++ Lesson_8/Home work/HW_8_1/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int[] SumLineElements(int[,] arr)
+{
+    int[] sumLine = new int[arr.GetLength(0)];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sumLine[i] += arr[i, j];
+        }
+    }
+    return sumLine;
+}
+
+void FindMin(int[] arr)
+{
+    int min_num = 0;
+    for (int i = 1; i < arr.Length; i++)
+        if (arr[i] < arr[min_num]) min_num = i;
+    Console.WriteLine($"Sum of elements: {arr[min_num]}, line: {min_num + 1}");
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+
+int[] muss_sum = SumLineElements(mass);
+FindMin(muss_sum);
