efaa14950f30c2144f457b5ce8cc1af5598000f7
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-25 15:15:31 +0700

    Tasks 9

---
+++ Lesson_9/9_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2. Задайте значения M и N. Напишите программу, которая 
+//    рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void ValuesSet(int M, int N)
+{
+    if (N < M) return;
+    ValuesSet(M , N- 1);
+    Console.Write($"{N} ");
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+ValuesSet(num1, num2);
---
+++ Lesson_9/9_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// 4. Напишите программу, которая на вход принимает два числа A и B,
+//    и возводит число А в целую степень B с помощью рекурсии.
+
+int ValuesSet(int A, int B)
+{
+    if (B == 0) return 1;
+    return ValuesSet(A, B - 1) * A;
+
+
+}
+
+int numA = int.Parse(Console.ReadLine()!);
+int numB = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(ValuesSet(numA, numB));
