20299b4001e613cf980fb76029835eeea259b445
Anastasia_Sol <an.solodova29@gmail.com>
2023-03-25 08:27:07 +0700

    Tasks HW_7_1_Attempt3

--- Lesson_7/Home Work/HW_7_1/Program.cs
+++ Lesson_7/Home Work/HW_7_1/Program.cs
@@ -6,28 +6,30 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-void Print(int[,] arr, int M, int N)
+int[,] FillArray(int[,] Array, int M, int N)
 {
-    int row = arr.GetLength(0);
-    int column = arr.GetLength(1);
-
     for (int i = 0; i < M; i++)
     {
         for (int j = 0; j < N; j++)
-            Console.Write($" {arr[i, j]} ");
-        Console.WriteLine();
+        {
+            Array[i, j] = new Random().Next(-10, 11);
+        }
     }
-    Console.WriteLine();
+    return (Array);
 }
 
-int[,] MassNums(int[,] arr, int M, int N)
+void PrintArray(int[,] Array, int M, int N)
 {
-    for (int i = 0; i < M; i++)
     {
-        for (int j = 0; j < N; j++)
-            arr[i, j] = new Random().Next(-10, 11);
+        for (int k = 0; k < M; k++)
+        {
+            for (int l = 0; l < N; l++)
+            {
+                Console.Write(Array[k, l] + " ");
+            }
+            Console.WriteLine();
+        }
     }
-    return arr;
 }
 
 void PositionSelection(int[,] Array, int NumberM, int NumberN, int M, int N)
@@ -35,7 +37,7 @@ void PositionSelection(int[,] Array, int NumberM, int NumberN, int M, int N)
     if (NumberM >= M | NumberN >= N | NumberM < 0 | NumberN < 0)
     {
         Console.WriteLine();
-        Console.WriteLine("Указанные значения вне диапазона массива!");
+        Console.WriteLine("Указанные значения вне диапазона массива");
     }
     else
     {
@@ -45,14 +47,15 @@ void PositionSelection(int[,] Array, int NumberM, int NumberN, int M, int N)
     }
 }
 
-Console.WriteLine("Enter amount of rows: ");
-int num_row = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Enter amount of columns: ");
-int num_column = int.Parse(Console.ReadLine()!);
+Console.Write("Введите колличество строк двумерного массива M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов двумерного массива N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 int[,] Array = new int[M, N];
-MassNums(array, M, N);
+FillArray(Array, M, N);
 
-Print(array, M, N);
+PrintArray(Array, M, N);
 
 Console.WriteLine();
 Console.Write($"Введите номер строки элемента массива от 0 до {M - 1}: ");
