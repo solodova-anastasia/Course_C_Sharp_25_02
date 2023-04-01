b2d549bb82f41d42c5bc96c0cc305fed96ec7e5b
Anastasia_Sol <an.solodova29@gmail.com>
2023-04-01 12:07:34 +0700

    Task _HW_9_0

--- Lesson_9/Home work/HW_9_0/Program.cs
+++ Lesson_9/Home work/HW_9_0/Program.cs
@@ -3,3 +3,20 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
+void EvenNumbers(int M, int N)
+{
+    if (M > N || M == 0 || N == 0) return;
+    if (M % 2 == 0)
+    {
+        Console.Write($"{M}; ");
+    }
+    EvenNumbers(M + 1, N);
+}
+
+
+Console.WriteLine("Введите целое положительное число M");
+int num_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите целое положительное число N");
+int num_2 = int.Parse(Console.ReadLine()!);
+
+EvenNumbers(num_1, num_2);
